2bf2af86ec29b3c4039d72d3b6a3bc1275b67c42
sheng <chengzhisheng2001@gmail.com>
2023-05-31 16:16:10 +0800

    feat: Add sidebar and header

--- src/components/CrowdCircle.tsx
+++ src/components/CrowdCircle.tsx
@@ -22,25 +22,26 @@ const CrowdCircle: React.FC<CrowdCircleProps> = ({
     return (
       <div
         onClick={onClick}
-        className="group flex flex-1 flex-col items-start justify-center rounded-2xl bg-cyan-700 px-4 py-6 shadow-md transition hover:scale-110 hover:cursor-pointer hover:shadow-lg active:scale-100"
+        className="group flex items-start justify-center gap-2 rounded-2xl bg-blue-500 px-4 py-6 shadow-md transition hover:scale-110 hover:cursor-pointer hover:shadow-lg active:scale-100"
       >
         {location.includes("Gym") ? (
-          <FaDumbbell className="h-16 w-12 text-white transition-all group-hover:text-orange-500" />
+          <FaDumbbell className="h-16 w-12 text-blue-200 transition-all group-hover:text-orange-500" />
         ) : (
-          <FaSwimmer className="h-16 w-16 text-white transition-all group-hover:text-cyan-500" />
+          <FaSwimmer className="h-16 w-16 text-blue-200 transition-all group-hover:text-cyan-500" />
         )}
+        <div className="flex w-48 flex-col text-white">
+          <div className="text-lg font-bold">{location}</div>
 
-        <div className="text-lg text-white font-bold">{location}</div>
+          <div className="mt-2 font-semibold">
+            {crowdLevel}/{capacity}
+          </div>
 
-        <div className="mt-2 font-semibold text-gray-200">
-          {crowdLevel}/{capacity}
-        </div>
-
-        <div className="mt-2 h-1 w-full bg-gray-200">
+          {/* <div className="mt-2 h-1 w-full bg-blue-500">
           <div
-            className="h-full bg-blue-500"
+            className="h-full bg-blue-900"
             style={{ width: `${occupancyPercentage}%` }}
           ></div>
+        </div> */}
         </div>
       </div>
     );
@@ -49,25 +50,26 @@ const CrowdCircle: React.FC<CrowdCircleProps> = ({
   return (
     <div
       onClick={onClick}
-      className="group flex flex-1 flex-col items-start justify-center rounded-2xl bg-white px-4 py-6 shadow-md transition hover:scale-110 hover:cursor-pointer hover:shadow-lg active:scale-100"
+      className="group flex items-start justify-center gap-2 rounded-2xl bg-white px-4 py-6 shadow-md transition hover:scale-110 hover:cursor-pointer hover:shadow-lg active:scale-100"
     >
       {location.includes("Gym") ? (
         <FaDumbbell className="h-16 w-12 text-gray-500 transition-all group-hover:text-orange-500" />
       ) : (
         <FaSwimmer className="h-16 w-16 text-gray-500 transition-all group-hover:text-cyan-500" />
       )}
+      <div className="flex w-48 flex-col">
+        <div className="text-lg font-bold">{location}</div>
 
-      <div className="text-lg font-bold">{location}</div>
+        <div className="mt-2 font-semibold text-gray-700">
+          {crowdLevel}/{capacity}
+        </div>
 
-      <div className="mt-2 font-semibold text-gray-700">
-        {crowdLevel}/{capacity}
-      </div>
-
-      <div className="mt-2 h-1 w-full bg-blue-500">
-        <div
-          className="h-full bg-blue-900"
-          style={{ width: `${occupancyPercentage}%` }}
-        ></div>
+        {/* <div className="mt-2 h-1 w-full bg-blue-500">
+          <div
+            className="h-full bg-blue-900"
+            style={{ width: `${occupancyPercentage}%` }}
+          ></div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { LOCATIONS, CAPACITIES } from "~/pages/constants";
+import CrowdCircle from "./CrowdCircle";
+import { FaMapMarkedAlt } from "react-icons/fa";
+
+interface SidebarProps {
+  index: number;
+  setIndex: (index: number) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ index, setIndex }) => {
+  return (
+    <div className="z-0 flex h-full w-48 flex-col justify-center gap-2 bg-blue-950 p-2">
+      <div className="flex items-center py-2">
+        <FaMapMarkedAlt className="h-4 w-8 text-white" />
+        <h1 className="text-3xl font-bold text-white">Locations</h1>
+      </div>
+      {LOCATIONS.map((location, i) => (
+        <CrowdCircle
+          key={location}
+          location={location}
+          capacity={CAPACITIES[i] ?? 100}
+          // crowdLevel={mockData[i]?.[0] ?? 0}
+          crowdLevel={10}
+          onClick={() => setIndex(i)}
+          selected={i === index}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
